e65df6a625df2c06d5126c7acbb2370414617e85
Tomasz Jonik <tomasz@hurricane.works>
2016-06-30 13:36:10 +0100

    Added missing exception implementation

--- src/Kemer/Amqp/Exceptions/DelayException.php
+++ src/Kemer/Amqp/Exceptions/DelayException.php
@@ -1,9 +1,25 @@
 <?php
 namespace Kemer\Amqp\Exceptions;
 
-/**
- * stub class representing AMQPExchangeException from pecl-amqp
- */
 class DelayException extends \Exception
 {
+    private $expiration;
+    private $retryCount;
+
+    public function __construct($expiration, $retryCount = 0)
+    {
+        $this->expiration = $expiration;
+        $this->retryCount = $retryCount;
+        parent::__construct("Message delayed: {$expiration} ms, {$retryCount} times");
+    }
+
+    public function getExpiration($expiration = 10000, $retryCount = null)
+    {
+        return $this->expiration;
+    }
+
+    public function getRetryCount()
+    {
+        return $this->retryCount;
+    }
 }
