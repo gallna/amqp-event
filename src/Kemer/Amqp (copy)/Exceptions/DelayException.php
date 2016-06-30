cc393a9f4307e64b7a435580cc92bbf042b16cc1
Tomasz Jonik <tomasz@hurricane.works>
2016-06-30 13:33:58 +0100

    Removed temporary code

--- src/Kemer/Amqp (copy)/Exceptions/DelayException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Kemer\Amqp\Exceptions;
-
-/**
- * stub class representing AMQPExchangeException from pecl-amqp
- */
-class DelayException extends \Exception
-{
-}
--- src/Kemer/Amqp (copy)/Exchange/Topic.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Kemer\Amqp\Exchange;
-
-interface Topic
-{
-
-}
